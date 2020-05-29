fa1a4775ec9979765f97ff1c5c6a90a04a8f2947
Geoff Goldrick <14170100+ProfStick@users.noreply.github.com>
2020-05-29 22:57:22 +1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../c4h_timers'))
+sys.path.insert(0, os.path.abspath('../../../c4h_timers'))
 
 
 # -- Project information -----------------------------------------------------
@@ -27,6 +27,7 @@ release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
